66cd9525cf0a68347443e028e3125008141a397f
DaiLoi2002 <loidai2002@gmail.com>
2023-10-09 22:16:48 +0700

    Sua bai Tuan4

---
+++ Tuan4/App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Bai1 from './BaiTap/Bai1';
+import Bai2 from './BaiTap/Bai2';
+
+export default function App() {
+  return (
+  // <Bai1/>
+  <Bai2/>
+  )
+}
+
+
