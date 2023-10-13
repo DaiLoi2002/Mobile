1fe91d2abcdd5bee808bb226df31567d8acd5d5c
DaiLoi2002 <loidai2002@gmail.com>
2023-10-13 11:29:17 +0700

    capnhattuan6

--- STT_15NguyenNgocDaiLoi_Tuan6/App.js
+++ STT_15NguyenNgocDaiLoi_Tuan6/App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import React from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 
+import { TextInput } from "react-native-web";
+
+import Bai1  from "./BaiTap/Bai1";
+import Bai2 from "./BaiTap/Bai2";
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    <Bai1/>
+    //<Bai2/>
+    
+  )
+};
