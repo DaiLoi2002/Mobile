63bf11d13a55f8c1aaa49f03b2c04b5727453b45
DaiLoi2002 <loidai2002@gmail.com>
2023-10-14 11:14:54 +0700

    lam bai tuan6 trua ngay 14/10

--- STT_15NguyenNgocDaiLoi_Tuan6/BaiTap/Bai1.js
+++ STT_15NguyenNgocDaiLoi_Tuan6/BaiTap/Bai1.js
@@ -6,124 +6,41 @@ import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import { TextInput } from "react-native-web";
 
 export default function Bai1() {
+  
   return (
     <View style={styles.container}>
       <View style={styles.top}>
-        <Text
-          style={{ fontWeight: "bold", fontSize: "60px", textAlign: "center" }}
-        >
-          {" "}
-          LOGIN{" "}
-        </Text>
+         <Image source={require('../assets/Vs.jpg')} style={styles.Image}></Image>
       </View>
-      <View style={styles.middle}>
-        <View>
-          <TextInput
-            style={styles.TextInput}
-            placeholder="Name"
-            textContentType="Username"
-            inputMode="text"
-          />
-          <Icon
-            name="user"
-            size={20}
-            color="black"
-            style={{ position: "absolute", left: 15, top: 10 }}
-          />
-          <View>
-            <TextInput
-              style={styles.TextInput}
-              placeholder="Password"
-              secerTextEntry={true}
-              inputMode="text"
-            />
-            <Icon
-              name="lock"
-              size={20}
-              color="black"
-              style={{ position: "absolute", left: 15, top: 10 }}
-            />
-
-            <Icon
-              name="eye"
-              size={20}
-              color="black"
-              style={{
-                position: "absolute",
-                right: 15,
-                top: 10,
-              }}
-            />
-          </View>
-        </View>
-      </View>
-      <View style={styles.middle1}>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.textButton}>LOGIN</Text>
-        </TouchableOpacity>
-
-        <Text style={styles.bottonText}>Forgot your Password?</Text>
-      </View>
+      <View style={{fontSize:15,
+    width:288,
+    height:18,
+    fontStyle:'normal',
+    fontFamily: 'Roboto',
+    lineHeight:17.58}}>Điện Thoại Vsmart Joy 3 - Hàng chính hãng</View>
     </View>
+
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundImage: "linear-gradient(190deg,#FBCB10,#BF9A40)",
+    backgroundImage: "#ffffff",
     width: 390,
   },
-  top: {
-    flex: 0.3,
-    alignItems: "flex-start",
-    justifyContent: "center",
+  Image:{
+    width:301,
+    height:361,
+    marginLeft:50,
+    marginTop:30
   },
-  middle: {
-    flex: 0.3,
-    alignItems: "center",
-    justifyContent: "space-evenly",
-  },
-  middle1: {
-    flex: 0.3,
-    alignItems: "center",
-    justifyContent: "space-evenly",
-  },
-  bottonText: {
-    fontWeight: "bold",
-    fontSize: "15px",
-    textAlign: "center",
-  },
-  button: {
-    width: "320px",
-    height: "50px",
-    boder: "1px solid #000000",
-    padding: "5px",
-    backgroundColor: "#000000",
-    borderRadius: "5px",
-  },
-  textButton: {
-    fontWeight: "bold",
-    textAlign: "center",
-    fontSize: "30px",
-    color: "#ffffff",
-  },
-  TextInput: {
-    height: 48,
-    width: 320,
-    margin: 1,
-    border: "1px solid #ffffff",
-    backgroundColor: "rgba(196,196,196,0.3)",
-    paddingLeft: "50px",
-    marginVertical: 4,
-  },
-  iconEmail: {
-    border: "3px solid #524f4e",
-    margin: 12,
-    height: 40,
-    backgroundColor: "#C4C4C4",
-  },
-  buttonlink: {
-    boder: "1px solid #000000",
-    width: 100,
-  },
+  text1:{
+    fontSize:15,
+    width:288,
+    height:18,
+    fontStyle:'normal',
+    fontFamily: 'Roboto',
+    lineHeight:17.58
+
+  }
 });
