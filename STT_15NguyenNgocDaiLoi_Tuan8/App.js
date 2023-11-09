5636c586531965347d8ff80293acef352f8cab35
DaiLoi2002 <loidai2002@gmail.com>
2023-11-09 21:01:41 +0700

    Up giao diện

--- STT_15NguyenNgocDaiLoi_Tuan8/App.js
+++ STT_15NguyenNgocDaiLoi_Tuan8/App.js
@@ -1,21 +1,23 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Screen1 from './Views/Screen1';
+import Screen2 from './Views/Screen2';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import screen1 from './src/screen1';
+import Screen3 from './Views/Screen3';
 
-
-
-const Stack = createNativeStackNavigator();
-
-function App() {
+var Stack = createNativeStackNavigator();
+function App({navigation}) {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={screen1} />
+        <Stack.Screen name="Screen1" component={Screen1} options={{headerShown: false}}/>
+        <Stack.Screen name="Screen2" component={Screen2} />
+        <Stack.Screen name="Screen3" component={Screen3} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
 export default App;
+
